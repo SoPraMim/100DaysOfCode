36c39bada87e161da1a9b7225f0cf65378e23938
Andre Dias <andreecdias@gmail.com>
2023-12-27 12:15:45 +0000

    Day 1
    - Created the band generator app from day 1.

---
+++ Day1 - Band Name Generator/main.py
@@ -0,0 +1,7 @@
+def main():
+    City=input("Welcome to the Band Name Generator.\nWhat's the name of the city you grew up in?\n")
+    Pet=input("What's your pet's name?\n")
+    print("Your band name could be",City, Pet)
+
+if __name__ == "__main__":
+    main()
