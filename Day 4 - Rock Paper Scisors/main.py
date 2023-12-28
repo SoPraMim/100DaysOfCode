74a95d018bb5a83e2dad6ad82b9b27829efe48d9
Andre Dias <74972999+SoPraMim@users.noreply.github.com>
2023-12-28 18:39:34 +0000

    Added day 4

--- Day 4 - Rock Paper Scisors/main.py
+++ Day 4 - Rock Paper Scisors/main.py
@@ -1,3 +1,30 @@
 import random
-n=random.randint(0,9)
-print(n)
+import my_module
+
+def main():
+    moves = ("rock","paper","scissors")
+    user_move = 0
+    while user_move == 0:
+        user_move = (input("Choose between \"Rock\", \"Paper\", \"Scissors.\n")).lower()
+        if user_move not in moves:
+            print("You typed a wrong move. Try again.")
+            user_move = 0
+    print(f"You selected {user_move}")
+    my_module.printMove(user_move)    
+    
+    ai_move = random.randint(0,2)
+    ai_move = moves[ai_move]
+    print (f"The AI selected {ai_move}.")
+    my_module.printMove(ai_move)
+    
+    winner = my_module.checkWinner(user_move,ai_move)
+    if winner == 0:
+        print("It was a draw. Try again!")
+    elif winner == 1:
+        print("Congrats! You won!")
+    elif winner == 2:
+        print("Oops! You lost. Try again!")
+    
+        
+if __name__ == "__main__":
+    main()
--- Day 4 - Rock Paper Scisors/my_module.py
+++ Day 4 - Rock Paper Scisors/my_module.py
@@ -1,2 +1,65 @@
-def pi()
-    return 3.14
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+def printMove(move):
+    if move == "rock":
+        print(rock)
+    elif move == "paper":
+        print(paper)
+    elif move == "scissors":
+        print(scissors)
+        
+def checkWinner(player1_move,player2_move):
+    if player1_move == "rock":
+        if player2_move == "rock":
+            return 0
+        elif player2_move == "paper":
+            return 2
+        elif player2_move == "scissors":
+            return 1
+        else:
+            print("Wrong move input")
+            
+    if player1_move == "paper":
+        if player2_move == "rock":
+            return 1
+        elif player2_move == "paper":
+            return 0
+        elif player2_move == "scissors":
+            return 2
+        else:
+            print("Wrong move input")
+
+    if player1_move == "scissors":
+        if player2_move == "rock":
+            return 2
+        elif player2_move == "paper":
+            return 1
+        elif player2_move == "scissors":
+            return 0
+        else:
+            print("Wrong move input")
