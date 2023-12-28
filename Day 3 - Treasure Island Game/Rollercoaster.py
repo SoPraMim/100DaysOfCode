47bb67f4ea440f91fd4764bc13616f4eaae15288
Andre Dias <74972999+SoPraMim@users.noreply.github.com>
2023-12-28 17:15:27 +0000

    Added day 3

---
+++ Day 3 - Treasure Island Game/Rollercoaster.py
@@ -0,0 +1,11 @@
+def main():
+    print("Welcome to the rollercoaster!")
+    height = int(input("What is your height in cm? "))
+
+    if height >= 120:
+        print("You can ride the rollercoaster")
+    else:
+        print("Sorry, you have to grow taller before you can ride.")
+
+if __name__ == "__main__":
+    main()
